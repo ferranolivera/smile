aa645b21d4c037dacb17403ed8b8cf12eebaee76
Arnau  Prat <aprat@ac.upc.edu>
2017-02-20 10:51:20 +0100

    fixed bug in macros.h with NON_COPYABLE macro

--- src/base/macros.h
+++ src/base/macros.h
@@ -6,4 +6,7 @@
 #define SMILE_NS_BEGIN namespace smile {
 #define SMILE_NS_END }
 
+#define SMILE_NON_COPYABLE( classname ) classname( const classname& ) = delete; \
+                                        classname& operator=(const classname& ) = delete;
+
 #endif
