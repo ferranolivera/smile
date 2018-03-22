dd246f025f96ad98270b89a31e46c801f026316d
Ferran Olivera <ferranolivera@gmail.com>
2018-03-22 17:50:58 +0100

    Parametrize page size in regtests and remove chrono usage.

--- src/regtests/performance_regtest.cpp
+++ src/regtests/performance_regtest.cpp
@@ -2,56 +2,47 @@
 #include <memory/buffer_pool.h>
 
 SMILE_NS_BEGIN
+
 /**
  * Tests an alloc operation of 4GB over a 1GB-size Buffer Pool for benchmarking purposes.
  */
 TEST(PerformanceTest, PerformanceTestAlloc) {
+	uint32_t PAGE_SIZE = 64;
 	BufferPool bufferPool;
-	ASSERT_TRUE(bufferPool.create(BufferPoolConfig{1024*1024}, "./test.db", FileStorageConfig{64}, true) == ErrorCode::E_NO_ERROR);
+	ASSERT_TRUE(bufferPool.create(BufferPoolConfig{1024*1024}, "./test.db", FileStorageConfig{PAGE_SIZE}, true) == ErrorCode::E_NO_ERROR);
 	BufferHandler bufferHandler;
 
-	auto start = std::chrono::system_clock::now();
-
-	for (uint64_t i = 0; i < 4*1024*1024; i += 64) {
+	for (uint64_t i = 0; i < 4*1024*1024; i += PAGE_SIZE) {
 		ASSERT_TRUE(bufferPool.alloc(&bufferHandler) == ErrorCode::E_NO_ERROR);
 		ASSERT_TRUE(bufferPool.unpin(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR);
 	}
-
-	auto end = std::chrono::system_clock::now();
-	std::chrono::duration<double> elapsed_seconds = end-start;
-	std::cout << "alloc_regtest = " << 4/elapsed_seconds.count() << std::endl;
 }
 
 /**
  * Tests a scan operation of 4GB over a 1GB-size Buffer Pool for benchmarking purposes.
  */
 TEST(PerformanceTest, PerformanceTestScan) {
+	uint32_t PAGE_SIZE = 64;
 	BufferPool bufferPool;
-	ASSERT_TRUE(bufferPool.create(BufferPoolConfig{1024*1024}, "./test.db", FileStorageConfig{64}, true) == ErrorCode::E_NO_ERROR);
+	ASSERT_TRUE(bufferPool.create(BufferPoolConfig{1024*1024}, "./test.db", FileStorageConfig{PAGE_SIZE}, true) == ErrorCode::E_NO_ERROR);
 	BufferHandler bufferHandler;
 
 	std::vector<pageId_t> pages;
 
 	// Allocate 4GB in disk before proceed scanning.
-	for (uint64_t i = 0; i < 4*1024*1024; i += 64) {
+	for (uint64_t i = 0; i < 4*1024*1024; i += PAGE_SIZE) {
 		ASSERT_TRUE(bufferPool.alloc(&bufferHandler) == ErrorCode::E_NO_ERROR);
 		ASSERT_TRUE(bufferPool.unpin(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR);
 		pages.push_back(bufferHandler.m_pId);
 	}
 
-	auto start = std::chrono::system_clock::now();
-
-	for (uint64_t i = 0; i < 4*1024*1024; i += 64) {
-		ASSERT_TRUE(bufferPool.pin(pages[i/64], &bufferHandler) == ErrorCode::E_NO_ERROR);
+	for (uint64_t i = 0; i < 4*1024*1024; i += PAGE_SIZE) {
+		ASSERT_TRUE(bufferPool.pin(pages[i/PAGE_SIZE], &bufferHandler) == ErrorCode::E_NO_ERROR);
 		ASSERT_TRUE(bufferPool.setPageDirty(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR);
 		uint64_t dataToWrite = i;
 		memcpy(bufferHandler.m_buffer, &dataToWrite, sizeof(uint64_t));
 		ASSERT_TRUE(bufferPool.unpin(bufferHandler.m_pId) == ErrorCode::E_NO_ERROR);
 	}
-
-	auto end = std::chrono::system_clock::now();
-	std::chrono::duration<double> elapsed_seconds = end-start;
-	std::cout << "scan_regtest = " << 4/elapsed_seconds.count() << std::endl;
 }
 
 SMILE_NS_END
